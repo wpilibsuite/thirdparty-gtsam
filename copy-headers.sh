8045f9c2d6a9c16131cc2307e5aab34858ac8f46
Jade Turner <spacey-sooty@proton.me>
2024-12-23 15:16:15 +0800

    Build everything properly
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- copy-headers.sh
+++ copy-headers.sh
@@ -1,3 +1,3 @@
 rm gradleDir/ -rf
 mkdir -p gradleDir/include/
-find build/_deps/gtsam-src/ \( -name "*.h" -or -name "*.hpp" \) -exec cp {} gradleDir/include \;
+find buildDebug/_deps/gtsam-src/ \( -name "*.h" -or -name "*.hpp" \) -exec cp {} gradleDir/include \;
